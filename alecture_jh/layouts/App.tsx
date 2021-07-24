fc67655dde7cf02f68c3a056ec4922a19e2f327f
ljh <dla1434@naver.com>
2021-07-25 03:36:25 +0900

    리액트 라우터 완료

--- alecture_jh/layouts/App.tsx
+++ alecture_jh/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-    return <div>좀만 참아주세요 ㅠㅠㅠ</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login"></Redirect>
+      <Route path="/login" component={LogIn}></Route>
+      <Route path="/signup" component={SignUp}></Route>
+    </Switch>
+  );
 };
 
 export default App;
