b75a1afef6bfe26186351614bdc192ab67f4ab4a
lim jong hyun <dla1434@naver.com>
2021-07-26 00:35:58 +0900

    기본 회원 가입

--- alecture_jh/pages/SignUp/index.tsx
+++ alecture_jh/pages/SignUp/index.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
-import { Form, Label, Input, LinkContainer, Button, Header } from './styles';
+import React, { useCallback, useState } from 'react';
+import { Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
+import { Link, Redirect } from 'react-router-dom';
 
 const SignUp = () => {
-  const [state, setState] = useState(initState);
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
+
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!mismatchError && nickname) {
+        console.log('서버로 회원가입하기');
+      }
+    },
+    [email, nickname, password, passwordCheck, mismatchError],
+  );
 
   return (
     <div id="container">
@@ -39,8 +78,8 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+          {/*{signUpError && <Error>{signUpError}</Error>}*/}
+          {/*{signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}*/}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
