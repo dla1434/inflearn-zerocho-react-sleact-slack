6ee432bd2a8b92a6538a461bce400e8c6f4b862e
lim jong hyun <dla1434@naver.com>
2021-07-28 22:32:07 +0900

    workspace 레이아웃 구성/gravatar로 프로필 랜덤이미지 생성

--- alecture_jh/layouts/WorkSpace/index.tsx
+++ alecture_jh/layouts/WorkSpace/index.tsx
@@ -1,8 +1,20 @@
 import fetcher from '@utils/fetcher';
 import React, { FC, useCallback } from 'react';
-import useSWR, { mutate } from 'swr';
+import useSWR from 'swr';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import {
+  Header,
+  RightMenu,
+  ProfileImg,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  Chats,
+  WorkspaceName,
+  MenuScroll,
+} from '@layouts/WorkSpace/styles';
+import gravatar from 'gravatar';
 
 const WorkSpace: FC = ({ children }) => {
   const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -23,7 +35,22 @@ const WorkSpace: FC = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogOut}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>Sidebar</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
