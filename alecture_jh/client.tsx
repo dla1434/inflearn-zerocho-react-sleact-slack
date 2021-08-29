0c89201000404a54c09ef690d5df1e94d3674ca3
lim jong hyun <dla1434@naver.com>
2021-08-30 01:21:11 +0900

    swr dev tools 적용

--- alecture_jh/client.tsx
+++ alecture_jh/client.tsx
@@ -1,11 +1,24 @@
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
-import App from '@layouts/App';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import SWRDevtools from '@jjordy/swr-devtools';
+import { cache, mutate } from 'swr';
+
+import App from './layouts/App';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
+console.log('env', process.env.NODE_ENV === 'production');
 
 render(
   <BrowserRouter>
-    <App />
+    <>
+      {process.env.NODE_ENV === 'production' ? null : <SWRDevtools cache={cache} mutate={mutate} />}
+      <App />
+    </>
   </BrowserRouter>,
   document.querySelector('#app'),
 );
