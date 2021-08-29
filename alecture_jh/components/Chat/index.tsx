76c49a69d6e379e97e02c5af1b538ff1764736b6
lim jong hyun <dla1434@naver.com>
2021-08-30 00:14:03 +0900

    드래그 앤 드랍 - 이미지 업로드

--- alecture_jh/components/Chat/index.tsx
+++ alecture_jh/components/Chat/index.tsx
@@ -1,15 +1,16 @@
-import React, { VFC, memo, useMemo } from 'react';
-import { IDM } from '@typings/db';
 import { ChatWrapper } from '@components/Chat/styles';
+import { IDM, IChat } from '@typings/db';
+import React, { VFC, memo, useMemo } from 'react';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
 import regexifyString from 'regexify-string';
 import { Link, useParams } from 'react-router-dom';
-import { IChat } from '../../../alecture/typings/db';
 
 interface Props {
   data: IDM | IChat;
 }
+
+const BACK_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3095' : 'https://sleact.nodebird.com';
 const Chat: VFC<Props> = ({ data }) => {
   const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = 'Sender' in data ? data.Sender : data.User;
@@ -20,26 +21,31 @@ const Chat: VFC<Props> = ({ data }) => {
   // ?는 0개나 1개
   const result = useMemo(
     () =>
-      regexifyString({
-        input: data.content,
-        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
-        decorator(match, index) {
-          const arr = match.match(/@\[(.+?)]\((\d+?)\)/)!;
-          //아이디가 있는 경우
-          console.log('arr', arr);
-          if (arr) {
-            console.log('exist', arr);
-            return (
-              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
-                @{arr[1]}
-              </Link>
-            );
-          }
-          //줄바꿈인 경우
-          //\n 줄바꿈을 br 태그로 바꿔주는 부분
-          return <br key={index} />;
-        },
-      }),
+      // uploads\\서버주소
+      data.content.startsWith('uploads\\') || data.content.startsWith('uploads/') ? (
+        <img src={`${BACK_URL}/${data.content}`} style={{ maxHeight: 200 }} />
+      ) : (
+        regexifyString({
+          input: data.content,
+          pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+          decorator(match, index) {
+            const arr = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+            //아이디가 있는 경우
+            console.log('arr', arr);
+            if (arr) {
+              console.log('exist', arr);
+              return (
+                <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                  @{arr[1]}
+                </Link>
+              );
+            }
+            //줄바꿈인 경우
+            //\n 줄바꿈을 br 태그로 바꿔주는 부분
+            return <br key={index} />;
+          },
+        })
+      ),
     [data.content],
   );
 
